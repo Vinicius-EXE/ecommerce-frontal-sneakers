476247b76995daebe4adfd88e889713059037348
tutsDev <arthurdev2707@gmail.com>
2025-11-10 08:36:06 -0300

    Spring Project criado

---
+++ frontal-sneakers-backend/src/main/java/com/ecommerce/frontalsneakers/FrontalsneakersApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.frontalsneakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontalsneakersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontalsneakersApplication.class, args);
+	}
+
+}
---
+++ frontal-sneakers-backend/src/test/java/com/ecommerce/frontalsneakers/FrontalsneakersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.frontalsneakers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontalsneakersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
