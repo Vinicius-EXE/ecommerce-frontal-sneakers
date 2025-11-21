65963096019ff4d3a9c712f8be9daaab4975555e
tutsDev <arthurdev2707@gmail.com>
2025-11-21 09:05:05 -0300

    Algumas rotas organizadas!

--- frontal-sneakers-frontend/src/app/app.ts
+++ frontal-sneakers-frontend/src/app/app.ts
@@ -1,14 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Adm } from "./components/adm/adm";
-import { Cabecalho } from "./components/cabecalho/cabecalho";
-import { Rodape } from "./components/rodape/rodape";
-import { AddEndereco } from "./components/add-endereco/add-endereco";
-import { AddCartao } from "./components/add-cartao/add-cartao";
-import { BuyTenis } from "./components/buy-tenis/buy-tenis";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Adm, Cabecalho, Rodape, AddEndereco, AddCartao, BuyTenis],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ frontal-sneakers-frontend/src/app/components/products-carousel/products-carousel.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
+
+@Component({
+  selector: 'app-products-carousel',
+  imports: [],
+  templateUrl: './products-carousel.html',
+  styleUrl: './products-carousel.css',
+})
+export class ProductsCarousel {
+
+}
